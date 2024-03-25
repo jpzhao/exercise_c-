48a2022f331ea064936ce2db57ed9c3c255f693e
jpzhao <36928790@qq.com>
2024-03-25 17:55:25 +0800

    update

--- targer/nested.cpp
+++ targer/nested.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-const int Cities=5;
-const int Years=4;
+#include <string>
+#include "queuetp.h"
+
 int main(){
-    using namespace std;
-    const char *cities[Cities]={
-        "Gribble City",
-        "Gribbletown",
-        "New Gribble",
-        "San Gribble",
-        "Gribble Vista"
-    };
+    using std::string;
+    using std::cin;
+    using std::cout;
 
-    int maxtemps[Years][Cities]={
-        {96,100,87,101,105},
-        {96,98,91,107,104},
-        {97,101,93,101,107},
-        {98,103,95,109,108}
-    };
-
-    cout<<"Maximum temperatures for 2008-2011\n\n";
-    for(int city=0;city<Cities;++city){
-        cout<<cities[city]<<":\t";
-        for(int year=0;year<Years;++year)
-            cout<<maxtemps[year][city]<<"\t";
-        cout<<endl;
+    QueueTP<string> cs(5);
+    string temp;
+    while(!cs.isfull()){
+        cout<<"Please enter your name.You will be"
+            "served in the order of arrival.\n"
+            "name: ";
+        getline(cin,temp);
+        cs.enqueue(temp);
     }
+
+    cout<<"The queue is full.Processing begins!\n";
+
+    while(!cs.isempty()){
+        cs.dequeue(temp);
+        cout<<"Now processing "<<temp<<"...\n";
+    }
+
     return 0;
 }
